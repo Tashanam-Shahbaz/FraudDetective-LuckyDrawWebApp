a79907e3866c8aa413fe1d631deb3f2f6ae3b217
Tashanam-Shahbaz <tashanamshahbaz72@gmail.com>
2024-01-07 23:42:02 +0500

    class_and er_daigram generated

--- frudDetection/frudDetection/settings.py
+++ frudDetection/frudDetection/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'fruddecApp',
+    'django_extensions',
+
 ]
 
 MIDDLEWARE = [
@@ -140,3 +142,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPH_MODELS = {
+  'all_applications': True,
+  'group_models': True,
+}
